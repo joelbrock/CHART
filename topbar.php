944039d66d9017af9c4dfb27715b7a807f51fa1e
Joel Brock <joelbrock@gmail.com>
2014-01-05 16:11:18 -0500

    track menu uses acroymize, should use code if available

--- topbar.php
+++ topbar.php
@@ -80,7 +80,8 @@ echo "<div class='track_btn'><ul class='dropdown'><li><a href='#'>TRACK!</a><ul 
 $clientr = mysql_query("SELECT * FROM clients LEFT JOIN staff_clients ON clients.id=staff_clients.clientID WHERE staff_clients.staffID='{$userinfo['id']}' ORDER BY clients.name");
 
 while ($clientRow = mysql_fetch_assoc($clientr)) {
-	echo "<li><a href='entry.php?clientID=".$clientRow['id']."'>".acronymize($clientRow['name'])."</a></li>";
+	$ccode = ($clientRow['code'] == '') ? acronymize($clientRow['name']) : $clientRow['code'];
+	echo "<li><a href='entry.php?clientID=".$clientRow['id']."'>$ccode</a></li>";
 }
 echo "</ul></li></ul></div>";
 
