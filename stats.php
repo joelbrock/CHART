debf97b411c55b337e3b183437080d0ff66bd745
Joel Brock <joelbrock@gmail.com>
2014-04-06 15:55:35 -0700

    alpha:  checking in NEW stats page.  *Very* alpha still and not 100% accurate.  Makes use of a more detailed attendance tbl using cielntIDs rather than parsed text to make true relational lookups for attendance data.

---
+++ stats.php
@@ -0,0 +1,136 @@
+<?php
+require("mysql_connect.php");
+
+include_once('topbar.php');
+
+
+?>
+<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<head>
+<link rel="stylesheet" href="css/style.css" type="text/css" charset="utf-8">
+<!--<link rel="stylesheet" href="css/tablesort.css" type="text/css" charset="utf-8">-->
+<script src="js/jquery.js" type="text/javascript" charset="utf-8"></script>
+<script src="js/jquery-ui.js" type="text/javascript" charset="utf-8"></script>
+
+</head>
+<body>
+
+<div id="wrapper">
+<div id="client_profile">
+<br /><br /><br />
+<?php
+$ctR = mysql_query("SELECT coop FROM attendance WHERE year = YEAR(CURDATE()) GROUP BY coop");
+$ct = mysql_num_rows($ctR);
+
+
+$ctNR = mysql_query("SELECT a.coop FROM attendance a, ncga_coops n WHERE a.clientID = n.clientID AND n.in_ncga <> '' 
+	 AND a.year = YEAR(CURDATE()) GROUP BY a.coop");
+$ctN = mysql_num_rows($ctNR);
+
+array_push($inNCGA, $ctN);
+$ctNRt = mysql_query("SELECT coop_name FROM ncga_coops WHERE in_ncga <> ''");
+$ctNt = mysql_num_rows($ctNRt);
+
+
+$inCBLD = array();
+$ctCR = mysql_query("SELECT a.coop FROM attendance a, ncga_coops n WHERE a.clientID = n.clientID AND n.in_cbld <> '' 
+	 AND a.year = YEAR(CURDATE()) GROUP BY a.coop");
+$ctC = mysql_num_rows($ctCR);
+
+array_push($inCBLD, $ctC);
+$ctCRt = mysql_query("SELECT coop_name FROM ncga_coops WHERE in_cbld <> ''");
+$ctCt = mysql_num_rows($ctCRt);
+
+echo "<div class='stat_box'><p># of Co-ops</p><span class='big'>$ct</span></div>";
+echo "<div class='stat_box'><p># of NCGA Co-ops</p><span class='big'>$ctN</span><span class='of'>/$ctNt</span></div>";
+echo "<div class='stat_box'><p># of CBLD Co-ops</p><span class='big'>$ctC</span><span class='of'>/$ctCt</span></div>";
+
+$eventQ = "SELECT event_name, eventid FROM events WHERE YEAR(event_date) = YEAR(CURDATE()) ORDER BY event_date";
+$eventR = mysql_query($eventQ);
+
+echo "<table border=1 cellspacing=0 class='stats_tbl'><tr><td width=160></td>";
+$events = array();
+while ($col = mysql_fetch_assoc($eventR)) {
+	array_push($events, $col['eventid']);
+	echo "<th>" . $col['event_name'] . "</th>";
+}
+// print_r($events);
+echo "</tr><tr><td class='label'>Registrants</td>";
+foreach($events as $e) {
+	// echo "eventID: " . $e;
+	$regR = mysql_query("SELECT COUNT(coop) FROM attendance WHERE eventID = $e AND year = YEAR(CURDATE())");
+	$reg = mysql_fetch_row($regR);
+	echo "<td class='data'>" . $reg[0] . "</td>";	
+}
+
+echo "</tr><tr><td class='label'>Attended</td>";
+foreach($events as $e) {
+	$attR = mysql_query("SELECT COUNT(coop) FROM attendance WHERE eventID = $e AND att <> '' AND year = YEAR(CURDATE())");
+	$att = mysql_fetch_row($attR);
+	echo "<td class='data'>" . $att[0] . "</td>";
+}
+
+echo "</tr><tr><td colspan=50>&nbsp;</td>";
+
+echo "</tr><tr><td class='label'>Board Member</td>";
+foreach($events as $e) {
+	$dirR = mysql_query("SELECT COUNT(coop) FROM attendance WHERE eventID = $e AND title IN('Director','Office','Chair/President') AND year = YEAR(CURDATE())");
+	$dir = mysql_fetch_row($dirR);
+	echo "<td class='data'>" . $dir[0] . "</td>";
+}
+
+echo "</tr><tr><td class='label'>Staff Member</td>";
+foreach($events as $e) {
+	$staR = mysql_query("SELECT COUNT(coop) FROM attendance WHERE eventID = $e AND title IN('GM','Other') AND year = YEAR(CURDATE())");
+	$sta = mysql_fetch_row($staR);
+	echo "<td class='data'>" . $sta[0] . "</td>";
+}
+
+echo "</tr><tr><td colspan=50>&nbsp;</td>";
+
+echo "</tr><tr><td class='label'># of Co-ops</td>";
+foreach($events as $e) {
+	$ctR = mysql_query("SELECT coop FROM attendance WHERE eventID = $e AND year = YEAR(CURDATE()) GROUP BY coop");
+	$ct = mysql_num_rows($ctR);
+	echo "<td class='data'>" . $ct . "</td>";
+}
+
+echo "</tr><tr><td class='label'># of NCGA Co-ops</td>";
+$inNCGA = array();
+foreach($events as $e) {
+	$ctNR = mysql_query("SELECT a.coop FROM attendance a, ncga_coops n WHERE a.clientID = n.clientID AND n.in_ncga <> '' 
+		AND a.eventID = $e AND a.year = YEAR(CURDATE()) GROUP BY a.coop");
+	$ctN = mysql_num_rows($ctNR);
+	echo "<td class='data'>" . $ctN . "</td>";
+	array_push($inNCGA, $ctN);
+}
+
+echo "</tr><tr><td class='label'># of CBLD Co-ops</td>";
+$inCBLD = array();
+foreach($events as $e) {
+	$ctCR = mysql_query("SELECT a.coop FROM attendance a, ncga_coops n WHERE a.clientID = n.clientID AND n.in_cbld <> '' 
+		AND a.eventID = $e AND a.year = YEAR(CURDATE()) GROUP BY a.coop");
+	$ctC = mysql_num_rows($ctCR);
+	echo "<td class='data'>" . $ctC . "</td>";
+	array_push($inCBLD, $ctC);
+	
+}
+
+echo "</tr><tr>";
+
+echo "</tr></table>";
+echo "<br /><br />";
+
+$listR = mysql_query("select a.coop, n.in_ncga, n.in_cbld FROM attendance a LEFT OUTER JOIN ncga_coops n ON a.clientID = n.clientID group by a.coop");
+echo "<table border=1 cellspacing=0>";
+while ($row = mysql_fetch_assoc($listR)) {
+	echo "<tr><td>".$row['coop']."</td><td>".$row['in_cbld']."</td><td>".$row['in_ncga']."</td></tr>";
+}
+echo "</table>";
+
+?>
+</div></div>
+</body>
+</html>
