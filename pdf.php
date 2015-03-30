ef7a9aa52a430a03cf5055c3f83d72ac31d587ee
joel brock <joelbrock@gmail.com>
2015-03-30 11:06:37 -0700

    Q1 2015 report page 2

--- pdf.php
+++ pdf.php
@@ -149,7 +149,7 @@ function content_filter($rc) {
 		$this->Ln(6);
 		$this->SetFont('Arial','B',10);
 		$this->Cell(8);
-		$this->PutLink("http://library.cdsconsulting.coop/article/ncg-consumer-advocacy-and-gmo-labeling/", "Great Idea: Whaddya Call it? Co-op governance!");
+		$this->PutLink("http://library.cdsconsulting.coop/article/great-idea-whaddaya-call-it-co-op-governance/", "Great Idea: Whaddya Call it? Co-op governance!");
 		$this->Ln(4);
 		$this->Cell(8);
 		$this->SetFont('Arial','I',9);
