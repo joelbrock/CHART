6030c24ba81fd91147f8fbf7a9707724db666faa
Joel Brock <joelbrock@cdsconsulting.coop>
2016-09-07 16:00:43 -0700

    remove restriction on reporting other consultant hours, fix for #21

--- pdf.php
+++ pdf.php
@@ -464,11 +464,13 @@ function Report($client,$filename,$dest='I')
 			$thatY = ($_GET['thatY']) ? $_GET['thatY'] : date('Y');
 			$staffnameR = mysql_query("SELECT s.firstname, s.lastname FROM staff s, staff_clients c WHERE s.id = c.StaffID AND c.ClientID = ".$clientID);
 			$cons_ct = mysql_num_rows($staffnameR);
-			$hours_ty = "SELECT SUM(Hours) FROM journal WHERE ClientID = " . $clientID . (($admin==false||$cons_ct==1)?" AND StaffID='{$userinfo['id']}'":'') . " AND YEAR(Date) = $thatY AND (MONTH(Date) < (".(3*($thisQ-1)+1).")) AND Category <> 'reset' AND Billable <> 0";//hours used this year prior to Q
+			//$hours_ty = "SELECT SUM(Hours) FROM journal WHERE ClientID = " . $clientID . (($admin==false||$cons_ct==1)?" AND StaffID='{$userinfo['id']}'":'') . " AND YEAR(Date) = $thatY AND (MONTH(Date) < (".(3*($thisQ-1)+1).")) AND Category <> 'reset' AND Billable <> 0";//hours used this year prior to Q
+			$hours_ty = "SELECT SUM(Hours) FROM journal WHERE ClientID = " . $clientID . " AND YEAR(Date) = $thatY AND (MONTH(Date) < (".(3*($thisQ-1)+1).")) AND Category <> 'reset' AND Billable <> 0";//hours used this year prior to Q			
 			$hours_tyr = mysql_query($hours_ty);
 			$client['hours_ty'] = mysql_fetch_row($hours_tyr);
 			$client['hrs']['total'] = round($client['hours_ty']['0'],2);
-			$hoursq = "SELECT *, DATE_FORMAT(`Date`,'%c/%e/%Y') as created_fmt FROM journal WHERE ClientID = " . $clientID . ($admin==false?" AND StaffID='{$userinfo['id']}'":'') . " AND YEAR(Date) = $thatY AND (MONTH(Date) >= (".(3*($thisQ-1)+1).") AND MONTH(Date)<=(".(3*$thisQ).")) AND Billable = 1 ORDER BY Category='quarterly' DESC, Date DESC ";
+			//$hoursq = "SELECT *, DATE_FORMAT(`Date`,'%c/%e/%Y') as created_fmt FROM journal WHERE ClientID = " . $clientID . ($admin==false?" AND StaffID='{$userinfo['id']}'":'') . " AND YEAR(Date) = $thatY AND (MONTH(Date) >= (".(3*($thisQ-1)+1).") AND MONTH(Date)<=(".(3*$thisQ).")) AND Billable = 1 ORDER BY Category='quarterly' DESC, Date DESC ";
+			$hoursq = "SELECT *, DATE_FORMAT(`Date`,'%c/%e/%Y') as created_fmt FROM journal WHERE ClientID = " . $clientID . " AND YEAR(Date) = $thatY AND (MONTH(Date) >= (".(3*($thisQ-1)+1).") AND MONTH(Date)<=(".(3*$thisQ).")) AND Billable = 1 ORDER BY Category='quarterly' DESC, Date DESC ";
 			$hoursr = mysql_query($hoursq);
 			if(mysql_num_rows($hoursr)==0)continue;
 			$hoursQ=0;
